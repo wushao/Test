88b5ebaf5c6b7de5f1f0fef5f349a1a1ba9c13f7
chenshaoxia <1085471562@qq.com>
2019-06-28 22:15:25 +0800

    WebCore初试MVC

--- WebCore/Startup.cs
+++ WebCore/Startup.cs
@@ -24,6 +24,7 @@ namespace WebCore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +39,7 @@ namespace WebCore
             //app.UseDefaultFiles();
             //app.UseStaticFiles();
             app.UseFileServer();
+            app.UseMvcWithDefaultRoute();
             app.Run(async (context) =>
             {
                
