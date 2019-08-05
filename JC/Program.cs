6ccfe8985ae2207ccab30b028503a0ce2a3a4b29
chenshaoxia <670708633@qq.com>
2019-08-05 10:03:23 +0800

    继承

---
+++ JC/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = TestManager.Msg();
+            Test1 test1 = (Test1)test;
+            test.Msg();
+            test1.Msg();
+            Console.ReadKey();
+        }
+    }
+
+    public class TestManager
+    {
+        public static Test Msg()
+        {
+            ObjectHandle handler = Activator.CreateInstance(null, "JC.Test1");
+            Object obj = handler.Unwrap();
+            var test = (Test)obj;
+            return test;
+;        }
+    }
+
+    public class Test
+    {
+        public virtual void Msg()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+
+    public class Test1 : Test
+    {
+        public override void Msg()
+        {
+            Console.WriteLine("Test1");
+        }
+    }
+}
